1da669bb614bb81ed32e622628ecc696faf6681b
desan <desanctisisabella95@gmail.com>
2025-03-16 19:54:00 +0100

    commit terzo

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        Forma f = new Forma();
+        Rettangolo ret = new Rettangolo();
+
+        f.calcolaArea();
+        ret.calcolaArea();
+
+    }
+}
---
+++ Rettangolo.java
@@ -0,0 +1,7 @@
+public class Rettangolo extends Forma{
+
+    @Override
+    public void calcolaArea(){
+        System.out.println("L'area del rettangolo è : base * altezza ");
+    }
+}
