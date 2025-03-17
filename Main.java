c8ccf7046e0982bad2eb975596efad51a1fcd382
desan <desanctisisabella95@gmail.com>
2025-03-17 12:42:31 +0100

    quarto commit

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ public class Main {
     public static void main(String[] args){
 
         Forma f = new Forma();
-        Rettangolo ret = new Rettangolo();
+        //double base;
+        //double altezza;
+        Rettangolo ret = new Rettangolo(10.0,20.8);
 
         f.calcolaArea();
         ret.calcolaArea();
--- Rettangolo.java
+++ Rettangolo.java
@@ -1,7 +1,27 @@
 public class Rettangolo extends Forma{
+    private double base;
+    private double altezza;
 
+    public Rettangolo(double base, double altezza){
+        this.base = base;
+        this.altezza = altezza;
+    }
+    public double getBase() {
+        return base;
+    }
+    public void setBase(double base){
+        this.base = base;
+    }
+    public double getAltezza(){
+        return altezza;
+    }
+
+    public void setAltezza(double altezza){
+        this.altezza=altezza;
+    }
     @Override
     public void calcolaArea(){
-        System.out.println("L'area del rettangolo è : base * altezza ");
+        double area = base * altezza;
+        System.out.println("L'area del rettangolo è : " + area);
     }
 }
