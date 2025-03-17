3f2c9a32a8397b3f02e37c742044709090e0c714
desan <desanctisisabella95@gmail.com>
2025-03-17 14:30:41 +0100

    quinto commit

--- Main.java
+++ Main.java
@@ -6,9 +6,9 @@ public class Main {
         //double base;
         //double altezza;
         Rettangolo ret = new Rettangolo(10.0,20.8);
-
+        Triangolo tri = new Triangolo(12.5, 3.4);
         f.calcolaArea();
         ret.calcolaArea();
-
+        tri.calcolaArea();
     }
 }
